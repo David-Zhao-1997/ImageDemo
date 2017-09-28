b855f0923bc3c163fddd1015e4dd6112ec8aca68
Inku <inkuxuan@gmail.com>
2017-09-28 17:31:26 +0800

    Git Test 02 By Inku final

--- src/test/GitTest.java
+++ src/test/GitTest.java
@@ -1,14 +1,16 @@
 package test;
 
-public class GitTest
-{
-    public static void main(String[] args)
-    {
-        System.out.println("change by no one");
-    }
+public class GitTest {
+	static private String strInku;
 
-    public static void DavidMethod()
-    {
+	public static void main(String[] args) {
+		System.out.println("change by no one");
+		strInku = "String Inku";
+		System.out.println("change by Inku, fuck David");
+		System.out.println(strInku);
+	}
 
-    }
+	public static void DavidMethod() {
+	}
+
 }
