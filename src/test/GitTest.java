d98060a7b54d9c7adb93643cae30a80b2d9ca366
David-Zhao-1997 <550361254@qq.com>
2017-09-28 17:36:49 +0800

    Git Test 04 By David

--- src/test/GitTest.java
+++ src/test/GitTest.java
@@ -4,8 +4,8 @@ public class GitTest {
 	static private String strInku;
 
 	public static void main(String[] args) {
-		System.out.println("change by no one");
-		strInku = "String Inku";
+        System.out.println("really change by david");
+        strInku = "String Inku";
 		System.out.println("change by Inku, fuck David");
 		System.out.println(strInku);
 	}
