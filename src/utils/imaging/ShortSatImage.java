62c7e1b40e3615a5069fdb12d57cca734e8e44b6
David-Zhao-1997 <550361254@qq.com>
2017-11-10 22:41:37 +0800

    【bug修复】    修复了Statistics中错误的setVariance()和setAverage()

--- src/utils/imaging/ShortSatImage.java
+++ src/utils/imaging/ShortSatImage.java
@@ -2,199 +2,237 @@ package utils.imaging;
 
 import java.util.ArrayList;
 
-public class ShortSatImage {
+public class ShortSatImage
+{
 
-	private ArrayList<short[][]> bands;
-	private Statistics stats;
+    private ArrayList<short[][]> bands;
+    private Statistics stats;
 
-	private int bandCount;
-	private int samples;
-	private int lines;
-	private int xStart;
-	private int yStart;
-	private double lon;
-	private double lat;
-	private double deltaLon;
-	private double deltaLat;
+    private int bandCount;
+    private int samples;
+    private int lines;
+    private int xStart;
+    private int yStart;
+    private double lon;
+    private double lat;
+    private double deltaLon;
+    private double deltaLat;
 
-	public ShortSatImage(int bandCount, int samples, int lines, int xStart, int yStart, double lon, double lat, double deltaLon, double deltaLat, ArrayList<short[][]> bands_) {
-		initBands(bands_);
-		this.bandCount = bandCount;
-		this.samples = samples;
-		this.lines = lines;
-		this.lon = lon;
-		this.lat = lat;
-		this.xStart = xStart;
-		this.yStart = yStart;
-		this.deltaLon = deltaLon;
-		this.deltaLat = deltaLat;
+    public ShortSatImage(int bandCount, int samples, int lines, int xStart, int yStart, double lon, double lat, double deltaLon, double deltaLat, ArrayList<short[][]> bands_)
+    {
+        initBands(bands_);
+        this.bandCount = bandCount;
+        this.samples = samples;
+        this.lines = lines;
+        this.lon = lon;
+        this.lat = lat;
+        this.xStart = xStart;
+        this.yStart = yStart;
+        this.deltaLon = deltaLon;
+        this.deltaLat = deltaLat;
 //		getStats();
-	}
+    }
 
-	/**
-	 * 对原图进行修改后传入的图像原型，请务必修改对应的参数，例如合并band后的bandCount！
-	 * @param origin
-	 * @param bands_
-	 */
-	public ShortSatImage(ShortSatImage origin, ArrayList<short[][]> bands_) {
-		initBands(bands_);
-		bandCount = origin.getBandCount();
-		samples = origin.getSamples();
-		lines = origin.getLines();
-		lon = origin.getLon();
-		lat = origin.getLat();
-		xStart = origin.getxStart();
-		yStart = origin.getyStart();
-		deltaLon = origin.getDeltaLon();
-		deltaLat = origin.getDeltaLat();
+    /**
+     * 对原图进行修改后传入的图像原型，请务必修改对应的参数，例如合并band后的bandCount！
+     *
+     * @param origin
+     * @param bands_
+     */
+    public ShortSatImage(ShortSatImage origin, ArrayList<short[][]> bands_)
+    {
+        initBands(bands_);
+        bandCount = origin.getBandCount();
+        samples = origin.getSamples();
+        lines = origin.getLines();
+        lon = origin.getLon();
+        lat = origin.getLat();
+        xStart = origin.getxStart();
+        yStart = origin.getyStart();
+        deltaLon = origin.getDeltaLon();
+        deltaLat = origin.getDeltaLat();
 //		getStats();
-	}
+    }
 
-	/**
-	 * 对原图进行修改后传入的图像原型，请务必修改对应的参数，例如合并band后的bandCount！
-	 * @param origin
-	 * @param bands_
-	 */
-	public ShortSatImage(SatImageFileHdr origin, ArrayList<short[][]> bands_) {
-		initBands(bands_);
-		bandCount = origin.getBandCount();
-		samples = origin.getSamples();
-		lines = origin.getLines();
-		lon = origin.getLon();
-		lat = origin.getLat();
-		xStart = origin.getxStart();
-		yStart = origin.getyStart();
-		deltaLon = origin.getDeltaLon();
-		deltaLat = origin.getDeltaLat();
+    /**
+     * 对原图进行修改后传入的图像原型，请务必修改对应的参数，例如合并band后的bandCount！
+     *
+     * @param origin
+     * @param bands_
+     */
+    public ShortSatImage(SatImageFileHdr origin, ArrayList<short[][]> bands_)
+    {
+        initBands(bands_);
+        bandCount = origin.getBandCount();
+        samples = origin.getSamples();
+        lines = origin.getLines();
+        lon = origin.getLon();
+        lat = origin.getLat();
+        xStart = origin.getxStart();
+        yStart = origin.getyStart();
+        deltaLon = origin.getDeltaLon();
+        deltaLat = origin.getDeltaLat();
 //		getStats();
-	}
+    }
 
-	private void initBands(ArrayList<short[][]> bands_) {
-		bands = bands_;
-	}
+    private void initBands(ArrayList<short[][]> bands_)
+    {
+        bands = bands_;
+    }
 
-	public short[][] getBand(int bandIndex) {
-		return bands.get(bandIndex - 1);
-	}
+    public short[][] getBand(int bandIndex)
+    {
+        return bands.get(bandIndex - 1);
+    }
 
-	private void getStats() {
-		if(stats == null) {
-			stats = new Statistics();
-			for (int x = 0; x < bands.size(); x++) {
-				long sum = 0;
-				double nXVariance = 0.0;
-				short[][] band = bands.get(x);
-				double avg = 0;
-				for (int i = 0; i < lines; i++) {
-					for (int j = 0; j < samples; j++) {
-						sum += band[i][j];
-					}
-				}
-				avg = sum / (samples * lines);
-				stats.setAverage(x, avg);
-				for (int i = 0; i < lines; i++) {
-					for (int j = 0; j < samples; j++) {
-						nXVariance += (band[i][j] - avg) * (band[i][j] - avg);
-					}
-				}
-				stats.setVariance(x, nXVariance / samples / lines);
-			}
-		}
-	}
+    private void getStats()
+    {
+        if (stats == null)
+        {
+            stats = new Statistics();
+            for (int x = 0; x < bands.size(); x++)
+            {
+                long sum = 0;
+                double nXVariance = 0.0;
+                short[][] band = bands.get(x);
+                double avg = 0;
+                for (int i = 0; i < lines; i++)
+                {
+                    for (int j = 0; j < samples; j++)
+                    {
+                        sum += band[i][j];
+                    }
+                }
+                avg = sum / (samples * lines);
+                stats.setAverage(x, avg);
+                for (int i = 0; i < lines; i++)
+                {
+                    for (int j = 0; j < samples; j++)
+                    {
+                        nXVariance += (band[i][j] - avg) * (band[i][j] - avg);
+                    }
+                }
+                stats.setVariance(x, nXVariance / samples / lines);
+            }
+        }
+    }
 
-	/**
-	 * 获得某波段的统计平均值，波段编号从1开始
-	 * @param bandIndex
-	 * @return
-	 */
-	public double getAverage(int bandIndex){
-		getStats();
-		return stats.getAverages().get(bandIndex-1);
-	}
+    /**
+     * 获得某波段的统计平均值，波段编号从1开始
+     *
+     * @param bandIndex
+     * @return
+     */
+    public double getAverage(int bandIndex)
+    {
+        getStats();
+        return stats.getAverages().get(bandIndex - 1);
+    }
 
-	/**
-	 * 获得某波段的统计方差，波段编号从1开始
-	 * @param bandIndex
-	 * @return
-	 */
-	public double getVariance(int bandIndex){
-		getStats();
-		return stats.getVariances().get(bandIndex-1);
-	}
+    /**
+     * 获得某波段的统计方差，波段编号从1开始
+     *
+     * @param bandIndex
+     * @return
+     */
+    public double getVariance(int bandIndex)
+    {
+        getStats();
+        return stats.getVariances().get(bandIndex - 1);
+    }
 
-	public int getBandCount() {
-		return bandCount;
-	}
+    public int getBandCount()
+    {
+        return bandCount;
+    }
 
-	public void setBandCount(int bandCount) {
-		this.bandCount = bandCount;
-	}
+    public void setBandCount(int bandCount)
+    {
+        this.bandCount = bandCount;
+    }
 
-	public int getSamples() {
-		return samples;
-	}
+    public int getSamples()
+    {
+        return samples;
+    }
 
-	public void setSamples(int samples) {
-		this.samples = samples;
-	}
+    public void setSamples(int samples)
+    {
+        this.samples = samples;
+    }
 
-	public int getLines() {
-		return lines;
-	}
+    public int getLines()
+    {
+        return lines;
+    }
 
-	public void setLines(int lines) {
-		this.lines = lines;
-	}
+    public void setLines(int lines)
+    {
+        this.lines = lines;
+    }
 
-	public double getLon() {
-		return lon;
-	}
+    public double getLon()
+    {
+        return lon;
+    }
 
-	public void setLon(double lon) {
-		this.lon = lon;
-	}
+    public void setLon(double lon)
+    {
+        this.lon = lon;
+    }
 
-	public double getLat() {
-		return lat;
-	}
+    public double getLat()
+    {
+        return lat;
+    }
 
-	public void setLat(double lat) {
-		this.lat = lat;
-	}
+    public void setLat(double lat)
+    {
+        this.lat = lat;
+    }
 
-	public double getDeltaLon() {
-		return deltaLon;
-	}
+    public double getDeltaLon()
+    {
+        return deltaLon;
+    }
 
-	public void setDeltaLon(double deltaLon) {
-		this.deltaLon = deltaLon;
-	}
+    public void setDeltaLon(double deltaLon)
+    {
+        this.deltaLon = deltaLon;
+    }
 
-	public double getDeltaLat() {
-		return deltaLat;
-	}
+    public double getDeltaLat()
+    {
+        return deltaLat;
+    }
 
-	public void setDeltaLat(double deltaLat) {
-		this.deltaLat = deltaLat;
-	}
+    public void setDeltaLat(double deltaLat)
+    {
+        this.deltaLat = deltaLat;
+    }
 
-	public int getxStart() {
-		return xStart;
-	}
+    public int getxStart()
+    {
+        return xStart;
+    }
 
-	public void setxStart(int xStart) {
-		this.xStart = xStart;
-	}
+    public void setxStart(int xStart)
+    {
+        this.xStart = xStart;
+    }
 
-	public int getyStart() {
-		return yStart;
-	}
+    public int getyStart()
+    {
+        return yStart;
+    }
 
-	public void setyStart(int yStart) {
-		this.yStart = yStart;
-	}
+    public void setyStart(int yStart)
+    {
+        this.yStart = yStart;
+    }
 
-	public ArrayList<short[][]> getBands() {
-		return bands;
-	}
+    public ArrayList<short[][]> getBands()
+    {
+        return bands;
+    }
 }
--- src/utils/imaging/Statistics.java
+++ src/utils/imaging/Statistics.java
@@ -4,32 +4,46 @@ import java.util.ArrayList;
 
 public class Statistics
 {
-	private ArrayList<Double> averages;
-	private ArrayList<Double> variances;
+    private ArrayList<Double> averages;
+    private ArrayList<Double> variances;
 
-	public Statistics()
-	{
-		averages = new ArrayList<>();
-		variances = new ArrayList<>();
-	}
+    public Statistics()
+    {
+        averages = new ArrayList<>();
+        variances = new ArrayList<>();
+    }
 
-	public ArrayList<Double> getAverages()
-	{
-		return averages;
-	}
+    public ArrayList<Double> getAverages()
+    {
+        return averages;
+    }
 
-	public void setAverage(int index, double average)
-	{
-		this.averages.set(index, average);
-	}
+    public void setAverage(int index, double average)
+    {
+        if (this.averages.size() >= index)
+        {
+            this.averages.add(index, average);
+        }
+        else
+        {
+            this.averages.set(index, average);
+        }
+    }
 
-	public ArrayList<Double> getVariances()
-	{
-		return variances;
-	}
+    public ArrayList<Double> getVariances()
+    {
+        return variances;
+    }
 
-	public void setVariance(int index, double variances)
-	{
-		this.variances.set(index, variances);
-	}
+    public void setVariance(int index, double variances)
+    {
+        if (this.variances.size() >= index)
+        {
+            this.variances.add(index, variances);
+        }
+        else
+        {
+            this.variances.set(index, variances);
+        }
+    }
 }
